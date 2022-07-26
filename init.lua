c13b6b9c709352df22badff466fff29eb0c85890
Jacob Stannix <jakestannix at gmail.com>
2022-07-25 22:07:22 -0600

    Added support for rust lsp
    -- Also added auto complete
    -- added various snippet services to nvim config

--- init.lua
+++ init.lua
@@ -1,2 +1,2 @@
 require("d1024")
-require('rust-tools').setup({})
+
--- lua/d1024/packer.lua
+++ lua/d1024/packer.lua
@@ -4,9 +4,31 @@ return require('packer').startup(function()
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
   use 'folke/tokyonight.nvim'
+  -- Lsp Config
   use 'neovim/nvim-lspconfig'
   use 'simrat39/rust-tools.nvim'
 
+  -- Completion framework
+  use 'hrsh7th/nvim-cmp'
+
+  -- LSP completion source for nvim-cmp
+  use 'hrsh7th/cmp-nvim-lsp'
+
+  -- Snippet completion source for nvim-cmp
+  use 'hrsh7th/cmp-vsnip'
+
+  -- other usefull compleiton sources
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-buffer'
+
+
+  -- Snippet engine
+  use 'hrsh7th/vim-vsnip'
+
+  -- Fuzzy finder
+  -- Optional
+  use 'nvim-lua/popup.nvim'
+  use 'nvim-telescope/telescope.nvim'
   use 'nvim-lua/plenary.nvim'
   use 'mfussenegger/nvim-dap'
 end)
